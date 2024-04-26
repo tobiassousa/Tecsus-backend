1117f4fa870db36d3ffac4c970edc479aca7e75e
Ariane-Sousa <ariane.crisousa@gmail.com>
2024-04-25 23:57:16 -0300

    refactor: desc start-install.bat

--- tecsus/start-install.bat
+++ tecsus/start-install.bat
@@ -1,18 +1,18 @@
 @echo off
 
-REM Construir contêineres Docker
+REM Construir conteineres Docker
 echo Construindo contêineres Docker...
 docker-compose build
 
-REM Aplicar migrações do Django
+REM Aplicar migraçoes do Django
 echo Aplicando migrações do Django...
 docker-compose run web python manage.py makemigrate
 
-REM Aplicar migrações do Django
+REM Aplicar migracoes do Django
 echo Aplicando migrações do Django...
 docker-compose run web python manage.py migrate
 
-REM Criar superusuário
+REM Criar superusuario
 echo Criando superusuário...
 docker-compose run web python manage.py createsuperuser
 echo Superusuário criado com sucesso.
