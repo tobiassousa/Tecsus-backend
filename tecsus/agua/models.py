174ae52292f44ee96bb43e5768afd5229a493e98
Ariane-Sousa <ariane.crisousa@gmail.com>
2024-05-12 15:25:29 -0300

    refactor: aplicando modelo estrela no app agua

--- tecsus/agua/models.py
+++ tecsus/agua/models.py
@@ -1,31 +1,28 @@
 from django.db import models
 
-class ContratoAgua(models.Model):
-    id_contrato_agua = models.AutoField(primary_key=True)
-    fornecedor = models.CharField(max_length=10000)
-    num_instalacao = models.CharField(max_length=10000)
-    num_medidor = models.CharField(max_length=10000)
-    num_cliente = models.CharField(max_length=10000)
-    modalidade = models.CharField(max_length=10000)
-    num_contrato = models.CharField(max_length=10000)
-    tipo_pagto = models.CharField(max_length=10000)
-    email_agua = models.CharField(max_length=10000)
-    cidade = models.CharField(max_length=10000)
-    cod_ligacao_rgi = models.CharField(max_length=10000)
-    data_extra = models.JSONField()
 
-class ProAgua(models.Model):
-    id_pro_agua = models.AutoField(primary_key=True)
-    leitura_anterior = models.CharField(max_length=10000)
-    leitura_atual = models.CharField(max_length=10000)
-    consumo_agua_m3 = models.CharField(max_length=10000)
-    consumo_esgoto_m3 = models.CharField(max_length=10000)
-    vlr_agua = models.CharField(max_length=10000)
-    vlr_esgoto = models.CharField(max_length=10000)
-    vlr_total = models.CharField(max_length=10000)
-    num_instalacao = models.CharField(max_length=10000)
-    num_medidor = models.CharField(max_length=10000)
-    num_cliente = models.CharField(max_length=10000)
-    cod_ligacao_rgi = models.CharField(max_length=10000)
-    num_contrato = models.CharField(max_length=10000)
-    data_extra = models.JSONField()
+class FornecedorAgua(models.Model):
+    id_fornecedor_agua = models.AutoField(primary_key=True)
+    fornecedor = models.CharField(max_length=255)
+    cod_companhia = models.IntegerField()
+    planta = models.CharField(max_length=255)
+    codigo_de_ligacao_rgi = models.CharField(max_length=255, unique=True)
+
+
+class Endereco(models.Model):
+    id_endereco = models.AutoField(primary_key=True)
+    endereco_instalacao = models.CharField(max_length=255)
+    cidade = models.CharField(max_length=255)
+
+
+class FatoContratoAgua(models.Model):
+    id_contrato_agua = models.AutoField(primary_key=True)
+    codigo_de_ligacao_rgi = models.IntegerField()
+    id_endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+    consumo_agua_m3 = models.IntegerField()
+    consumo_esgoto_m3 = models.IntegerField()
+    vlr_agua = models.DecimalField(max_digits=10, decimal_places=2)
+    vlr_esgoto = models.DecimalField(max_digits=10, decimal_places=2)
+    vlr_total = models.DecimalField(max_digits=10, decimal_places=2)
+    leitura_anterior = models.DateField()
+    leitura_atual = models.DateField()
